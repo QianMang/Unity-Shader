a522354b61daed843355fb1d70554575daa88e4e
mangAI <2385914340@qq.com>
2019-05-03 12:49:33 -0400

    edgeDetection

---
+++ Post Proccessing Effect/EdgeDetection/EdgeDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetection : PostEffectsBase {
+
+    public Shader edgeDetectionShader;
+    private Material edgeDetectionMaterial;
+    public Material material
+    {
+        get
+        {
+            edgeDetectionMaterial = CheckShaderAndCreateMaterial(edgeDetectionShader, edgeDetectionMaterial);
+            return edgeDetectionMaterial;
+        }
+    }
+
+    [Range(0.0f, 1.0f)]
+    public float edgeOnly= 0.0f;
+
+    public Color edgeColor = Color.black;
+    public Color backgroundColor = Color.white;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+            print("123");
+            material.SetFloat("_EdgeOnly", edgeOnly);
+            material.SetColor("_EdgeColor", edgeColor);
+            material.SetColor("_BackgroundColor", backgroundColor);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+}
