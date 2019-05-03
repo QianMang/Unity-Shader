3c44801dde6773e736d6b5050196b982ba67ad2f
mangAI <2385914340@qq.com>
2019-05-02 21:32:19 -0400

    screen effect

---
+++ Post Proccessing Effect/Brightness_Saturation_Contrast/Brightness.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//BrightnessSaturationAndContrast
+
+public class Brightness : PostEffectsBase {
+    public Shader briSatConShader;
+    private Material briSatConMaterial;
+    public Material material{
+        get{
+            briSatConMaterial = CheckShaderAndCreateMaterial(briSatConShader, briSatConMaterial);
+            return briSatConMaterial;
+        }
+    }
+
+    [Range(0.0f, 3.0f)]
+    public float brightness = 1.0f;
+
+    //饱和度
+    [Range(0.0f, 3.0f)]
+    public float saturation = 1.0f;
+
+    [Range(0.0f, 3.0f)]
+    public float contrast = 1.0f;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(material!=null){
+            print("123");
+            material.SetFloat("_Brightness", brightness);
+            material.SetFloat("_Saturation", saturation);
+            material.SetFloat("_Contrast", contrast);
+
+            Graphics.Blit(source, destination, material);
+        }else{
+            Graphics.Blit(source, destination);
+        }
+    }
+}
